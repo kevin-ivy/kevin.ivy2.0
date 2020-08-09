dfcb48774dd2e04e408f1998195be9d4c1792e56
kIvy1189 <kevindivy@gmail.com>
2020-08-09 14:13:58 -0500

    Updates

---
+++ precache-manifest.0910167bf4d35cc1ef4c41c92b99ce91.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e32dc9223f43245c61da2bffadf3c0e",
+    "url": "/kevin.ivy2.0/index.html"
+  },
+  {
+    "revision": "91807f35a51d737c7583",
+    "url": "/kevin.ivy2.0/static/css/2.aa704b0f.chunk.css"
+  },
+  {
+    "revision": "1ce9bdb8001ae7ad1031",
+    "url": "/kevin.ivy2.0/static/css/main.3da4652d.chunk.css"
+  },
+  {
+    "revision": "91807f35a51d737c7583",
+    "url": "/kevin.ivy2.0/static/js/2.c04391f0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kevin.ivy2.0/static/js/2.c04391f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce9bdb8001ae7ad1031",
+    "url": "/kevin.ivy2.0/static/js/main.9577c831.chunk.js"
+  },
+  {
+    "revision": "7f0b0d32b919a38fe3c9",
+    "url": "/kevin.ivy2.0/static/js/runtime-main.9b3135aa.js"
+  },
+  {
+    "revision": "cdccd803cb9b2227b7c7481ff2f730ee",
+    "url": "/kevin.ivy2.0/static/media/background.cdccd803.png"
+  },
+  {
+    "revision": "e311a7e3e6ac97a9f0240e02aa871741",
+    "url": "/kevin.ivy2.0/static/media/budget.e311a7e3.png"
+  },
+  {
+    "revision": "54b4dd8d27c566bea4e88fd5f104e59c",
+    "url": "/kevin.ivy2.0/static/media/notes.54b4dd8d.png"
+  },
+  {
+    "revision": "e756ebd049230a021b2593b2830a16b5",
+    "url": "/kevin.ivy2.0/static/media/paperfinder.e756ebd0.png"
+  },
+  {
+    "revision": "2c53a33c9b3a7697088306f4e43b3d7f",
+    "url": "/kevin.ivy2.0/static/media/pizza.2c53a33c.png"
+  },
+  {
+    "revision": "1dcb78b5c60c58d5391aef73a4e0d46e",
+    "url": "/kevin.ivy2.0/static/media/runbuddy.1dcb78b5.png"
+  },
+  {
+    "revision": "fdaa698df0154c6c01808f15c2d036c9",
+    "url": "/kevin.ivy2.0/static/media/selfPortrait.fdaa698d.png"
+  },
+  {
+    "revision": "7b809ef1c40cb38043b945347c2c30ff",
+    "url": "/kevin.ivy2.0/static/media/updog.7b809ef1.png"
+  }
+]);
