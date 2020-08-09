dab653549ee537668ca4059624d271af374e0b86
kIvy1189 <kevindivy@gmail.com>
2020-08-09 11:41:16 -0500

    Updated form submission

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -16,7 +16,7 @@ function ContactForm() {
             }
         } else {
             if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
+                setErrorMessage(`${e.target.name} must be entered.`);
             } else {
                 setErrorMessage('');
             }
@@ -24,7 +24,13 @@ function ContactForm() {
 
         if (!errorMessage) {
             setFormState({...formState, [e.target.name]: e.target.value});
-        }
+        } 
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrorMessage('Form submitted successfully!');
+        console.log(formState);
     }
 
     return (
@@ -32,14 +38,19 @@ function ContactForm() {
             <div>
                 <h6 className='text-center'>Let me know if you're interested in partnering on your current or latest project.</h6>
             </div>
-            <form className='d-flex justify-content-center'>
-                <div class='form-group w-100' style={{maxWidth: '500px'}}>
+            <form className='d-flex justify-content-center' onSubmit={handleSubmit}>
+                <div className='form-group w-100' style={{maxWidth: '500px'}}>
                     <br/>
                     <input type='text' defaultValue={name} onBlur={handleChange} name='name' placeholder='Name' className='form-control m-3 mx-auto w-100'/>
                     <input type='text' defaultValue={email} onBlur={handleChange} name='email' placeholder='Email' className='form-control m-3 mx-auto w-100'/>
-                    <textarea defaultValue={message} onBlur={handleChange} name='message' placeholder='Message' rows='4' maxlength='250' className='form-control mt-3 mx-auto w-100'/>
+                    <textarea defaultValue={message} onBlur={handleChange} name='message' placeholder='Message' rows='4' maxLength='250' className='form-control mt-3 mx-auto w-100'/>
                     <br/>
-                    <button type="submit" className="btn btn-info mx-auto">Send Message</button>
+                    {errorMessage && (
+                    <div>
+                        <p className='error-text'>{errorMessage}</p>
+                    </div>
+                    )}
+                    <button type="submit" className="btn btn-info mt-2 mx-auto">Send Message</button>
                 </div>
             </form>
         </div>
